701754d509982d118d0b5d580d7e6d726742c175
Guy-Shalev <guy.shalev.email@gmail.com>
2024-09-12 19:10:22 +0300

    Initial commit

---
+++ IntuitHomeAssignment/src/main/java/com/guy/shalev/IntuitHomeAssignment/IntuitHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.guy.shalev.IntuitHomeAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuitHomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntuitHomeAssignmentApplication.class, args);
+	}
+
+}
